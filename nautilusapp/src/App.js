92f1bef594bd926bf54d0a37aaf43741555a88ac
aparna4scholas <aparna4scholas@gmail.com>
2020-03-28 23:29:49 -0700

    added files

---
+++ nautilusapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import matches from './matches.js';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      
+      </div>
+  );
+}
+
+export default App;
